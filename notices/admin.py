fa0e8ea54dfd9df1523f76f7be3694c8402cbae6
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-01-01 20:31:17 +0000

    Add URL at admin panel

--- notices/admin.py
+++ notices/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Notice
 
 
+def url(obj) -> str:
+    return mark_safe(f'<a target="blank" href="/{obj.uuid}/">/{obj.uuid}</a>')
+
+
 @admin.register(Notice)
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'recipient', 'body')
+    list_display = ('pk', url, 'recipient', 'body')
     exclude = ('uuid',)
