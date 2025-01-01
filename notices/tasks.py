ab560d45dde12283622ff9931aeb30170e346d1f
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-01-01 17:41:20 +0000

    Escape markdown

--- notices/tasks.py
+++ notices/tasks.py
@@ -1,7 +1,8 @@
 from django_rq import job
 from telegramtk import send_message
+from telegramtk.utils import escape_markdown
 
 
 @job
 def send_notice(notice) -> None:
-    send_message(notice.to, notice.body)
+    send_message(notice.to, escape_markdown(notice.body))
